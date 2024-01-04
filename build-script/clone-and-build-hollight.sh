ee1c4e34953337e9a1854867fc607fcc5ea2a8a3
Juneyoung Lee <aqjune@gmail.com>
2024-01-04 11:14:04 -0600

    Use ocaml 4.14

--- build-script/clone-and-build-hollight.sh
+++ build-script/clone-and-build-hollight.sh
@@ -1,15 +1,19 @@
 set -e
+if false; then
 git clone https://github.com/jrh13/hol-light.git
 cd hol-light
-opam switch create `pwd` 4.05.0
+opam switch create `pwd` 4.14.0
 eval $(opam env)
 
-opam pin add -y camlp5 7.10
+opam pin add -y camlp5 8.00.03
 opam install -y num
 
 eval $(opam env)
 make
+ocamlmktop -o ocaml-full
+fi
+cd hol-light
 cp ../run-hol-light-template ./run-hol-light
 sed -i "s|__DIR|$(pwd)|g" run-hol-light
-sed -i "s|__OCAML|ocaml|g" run-hol-light
+sed -i "s|__OCAML|./ocaml-full|g" run-hol-light
 chmod +x run-hol-light
