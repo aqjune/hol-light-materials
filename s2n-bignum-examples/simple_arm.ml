b7848c2b1ac6972a67bcfcff28128dd59fc30860
Juneyoung Lee <aqjune@gmail.com>
2024-01-04 12:24:00 -0600

    Update

--- s2n-bignum-examples/simple_arm.ml
+++ s2n-bignum-examples/simple_arm.ml
@@ -1,25 +1,27 @@
-(****** A simple specification about program 'loop.S' *******)
+(****** A simple specification about program 'simple_arm.S' *******)
 
-let loop_mc = define_assert_from_elf "loop_mc" "loop.o"
+needs "arm/proofs/base.ml";;
+
+let simple_arm_mc = define_assert_from_elf "simple_arm_mc" "simple_arm.o"
 [
   0xaa1f03e0;       (* arm_MOV X0 XZR *)
   0x91000400;       (* arm_ADD X0 X0 (rvalue (word 1)) *)
   0xf1000c1f;       (* arm_CMP X0 (rvalue (word 3)) *)
   0x54ffffc1        (* arm_BNE (word 2097144) *)
 ];;
-let LOOP_EXEC = ARM_MK_EXEC_RULE loop_mc;;
+let EXEC = ARM_MK_EXEC_RULE simple_arm_mc;;
 
 let SIMPLE_SPEC = prove(
   `!pc.
   ensures arm
-    (* Precondition *)
-    (\s. aligned_bytes_loaded s (word pc) loop_mc /\
+    // Precondition
+    (\s. aligned_bytes_loaded s (word pc) simple_arm_mc /\
          read PC s = word (pc+4) /\
          read X0 s = word 1)
-    (* Postcondition *)
+    // Postcondition
     (\s. read PC s = word (pc+8) /\
          read X0 s = word 2)
-    (* Registers (and memory locations) that may change after execution *)
+    // Registers (and memory locations) that may change after execution
     (MAYCHANGE [PC;X0] ,, MAYCHANGE SOME_FLAGS)`,
   (* Unravel ARM flag registers! *)
   REWRITE_TAC[SOME_FLAGS] THEN
@@ -27,7 +29,7 @@ let SIMPLE_SPEC = prove(
   (* Start symbolic execution with state 's0' *)
   ENSURES_INIT_TAC "s0" THEN
   (* Symbolically run one instruction *)
-  ARM_STEPS_TAC LOOP_EXEC (1--1) THEN
+  ARM_STEPS_TAC EXEC (1--1) THEN
   (* Try to prove the postcondition as much as possible *)
   ENSURES_FINAL_STATE_TAC THEN
   (* Some unproven parts can be proven using ASM_REWRITE_TAC[]. *)
