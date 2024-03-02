37238f5989785026556c7e740469f76f1590ff86
Juneyoung Lee <aqjune@gmail.com>
2024-03-02 14:19:24 -0600

    update build script

--- build-script/clone-and-build-hollight.sh
+++ build-script/clone-and-build-hollight.sh
@@ -1,17 +1,17 @@
 set -e
-if false; then
+
 git clone https://github.com/jrh13/hol-light.git
 cd hol-light
 opam switch create `pwd` 4.14.0
 eval $(opam env)
 
 opam pin add -y camlp5 8.00.03
-opam install -y num
+opam install -y num ledit
 
 eval $(opam env)
 make
 ocamlmktop -o ocaml-full
-fi
+
 cd hol-light
 cp ../run-hol-light-template ./run-hol-light
 sed -i "s|__DIR|$(pwd)|g" run-hol-light
