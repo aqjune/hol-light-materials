15d5a12a10daa45a3b0ab120fc88b62e14ac59e7
Juneyoung Lee <aqjune@gmail.com>
2024-04-09 18:44:46 -0500

    Update clone-and-build-hollight.sh

--- build-script/clone-and-build-hollight.sh
+++ build-script/clone-and-build-hollight.sh
@@ -6,14 +6,12 @@ opam switch create `pwd` 4.14.0
 eval $(opam env)
 
 opam pin add -y camlp5 8.00.03
-opam install -y num ledit
+opam install -y zarith ledit # If installing zarith fails, please install its dependency gmp using e.g., apt-get install -y libgmp-dev .
 
 eval $(opam env)
 make
-ocamlmktop -o ocaml-full
 
-cd hol-light
-cp ../run-hol-light-template ./run-hol-light
-sed -i "s|__DIR|$(pwd)|g" run-hol-light
-sed -i "s|__OCAML|./ocaml-full|g" run-hol-light
-chmod +x run-hol-light
+# Now you can use `hol.sh`.
+# When you are invoking `hol.sh` from a directory other than hol-light,
+# Please run `eval $(opam env --switch (the hol-light directory path) --set-switch)` first.
+# If you want to use ledit, please do `export LINE_EDITOR=ledit` before calling `hol.sh`.
